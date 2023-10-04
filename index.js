9da6020d507060903c50f3d5bf0b2a1439f70b96
BorisKutsenko <boris.k@theevenstar.net>
2023-10-04 20:52:14 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const socket = require('socket.io')
+
+const io = new socket.Server({
+    cors: 'http://localhost:5173'
+})
+
+io.listen('3001')
+
+const characters = []
+
+const generateRandomPosition = () => {
+    return [Math.random() * 3, 0, Math.random() * 3]
+}
+
+const generateRandomHexColor = () => {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16)
+}
+
+io.on('connection', socket => {
+    console.log('connected')
+
+    characters.push({
+        id: socket.id,
+        position: generateRandomPosition(),
+        bodyColor: generateRandomHexColor(),
+        headColor: generateRandomHexColor(),
+        feetColor: generateRandomHexColor(),
+    })
+
+    socket.emit('hello', 'message')
+
+    socket.on('move', (position) => {
+        const character = characters.find(character => character.id === socket.id)
+        character.position = position
+        io.emit('characters', characters)
+    })
+
+    io.emit('characters', characters)
+
+    socket.on('disconnect', () => {
+        console.log('disconnected')
+
+        characters.splice(
+            characters.findIndex(character => character.id === socket.id),
+            1
+        )
+        io.emit('characters', characters)
+    })
+})
